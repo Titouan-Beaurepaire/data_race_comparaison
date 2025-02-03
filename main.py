23ca08886ee2570dd36a24077d2b4d22195e1fe8
Titouan Beaurepaire <titouan-luigi-roland.beaurepaire@epitech.eu>
2025-02-03 13:35:37 +0100

    [ADD][get how many laps the pilot do]

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ def read_files():
         data = file.readlines()
     return data
 
+def get_lap(data):
+    last_value = None
+    for line in data:
+        columns = line.strip().split(',')
+        if len(columns) > 1:
+            last_value = columns[0]
+    if last_value:
+        print(last_value)
+    return last_value
+
 def get_laptime(data):
     for line in data:
         columns = line.strip().split(',')
@@ -19,3 +29,4 @@ if __name__ == "__main__":
     main()
     data = read_files()
     get_laptime(data)
+    get_lap(data)
